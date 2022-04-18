9bdd0cc5a8de8ef39b6e2f4ca89676ab365c7e24
Jeremy Daffix <jeremy.daffix@gmail.com>
2022-04-18 14:28:11 +0200

    repertoires pour le rendu + ajout du projet template Unity

---
+++ Unity/Assets/Scripts/Scorer.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace clicker
+{
+    public class Scorer : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _textbox;
+        private int _score;
+
+        private void OnEnable()
+        {
+            _textbox.SetText(_score.ToString());
+        }
+
+        public void IncreaseScore()
+        {
+            _score++;
+            _textbox.SetText(_score.ToString());
+        }
+    }
+}
