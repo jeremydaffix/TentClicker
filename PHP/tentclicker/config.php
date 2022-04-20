102cb0b0fc640c8b91527057b4df28036efe4f85
Jeremy Daffix <jeremy.daffix@gmail.com>
2022-04-20 16:56:28 +0200

    documentation code web

--- PHP/tentclicker/config.php
+++ PHP/tentclicker/config.php
@@ -1,11 +1,11 @@
 <?php
 
-	// configuration de l'accès à la base de données
+	// *** configuration de l'accès à la base de données ***
 	
-	$config_sgbdr = 'mysql';
-	$config_host = 'localhost';
-	$config_db = 'tentclicker';
-	$config_user = 'root';
-	$config_password = '';
+	$config_sgbdr = 'mysql'; // moteur de bdd
+	$config_host = 'localhost'; // adresse du serveur
+	$config_db = 'tentclicker'; // nom de la bdd
+	$config_user = 'root'; // nom utilisateur
+	$config_password = ''; // mot de passe utilisateur
 
 ?>
--- PHP/tentclicker/index.php
+++ PHP/tentclicker/index.php
@@ -1,14 +1,17 @@
 <?php
 
-	require_once 'config.php';
+	// *** script principal de création / récupération d'une sauvegarde de jeu ***
+
+	require_once 'config.php'; // variables de configuration de l'accès à la bdd
 	
-	$pdo = ConnectDatabase();
-	DispatchRequest();
+	$pdo = ConnectDatabase(); // connexion
+	DispatchRequest(); // gestion de la fonction de l'API à appeler selon le type de requête
 	
 	
 	// ********************
 
 
+	// fonction de connexion à la bdd selon la configuration
 	function ConnectDatabase()
 	{
 		global $config_sgbdr, $config_host, $config_db, $config_user, $config_password;
@@ -32,6 +35,10 @@
 		}
 	}
 
+	// fonction appelant le bon code selon le verbe http utilisé,
+	// dans la logique REST
+	// game/ + POST -> création
+	// game/:id (GET) -> récupération
 	function DispatchRequest()
 	{
 		$request_method = $_SERVER["REQUEST_METHOD"];
@@ -46,16 +53,18 @@
 				CreateGame();
 				break;
 				 
-			default:
+			default: // autre : retour d'une erreur http
 				header("HTTP/1.0 405 Method Not Allowed");
 				break;
 		}
 	}
 
+	// fonction de création d'une sauvegarde de jeu
 	function CreateGame()
 	{	
 		global $pdo;
 	
+		// vérification de l'existence et de la validité des paramètres passés
 		if(isset($_POST["score"]) && $_POST["score"] != "" &&
 		   isset($_POST["clickUpgradeLevel"]) && $_POST["clickUpgradeLevel"] != "" &&
 		   isset($_POST["autoGatherUpgradeLevel"]) && $_POST["autoGatherUpgradeLevel"] != "")
@@ -66,14 +75,15 @@
 			
 			//echo "$score  $clickUpgradeLevel  $autoGatherUpgradeLevel";
 			
-			$id = GenerateID();
+			$id = GenerateID(); // identifiant "aléatoire" sur 6 caractères
 			
-			$sql = "INSERT INTO game VALUES(:id, :score, :clickUpgradeLevel, :autoGatherUpgradeLevel)";
+			$sql = "INSERT INTO game VALUES(:id, :score, :clickUpgradeLevel, :autoGatherUpgradeLevel)"; // code requête préparée
 
 			try
 			{
 				$statement = $pdo->prepare($sql);
 
+				// exécution de la requête préparée
 				$statement->execute([
 					':id' => $id,
 					':score' => $score,
@@ -83,9 +93,10 @@
 				
 				$statement = null;
 				
-				die($id);
+				die($id); // on retourne au client l'identifiant de la sauvegarde créée
 			}
 			
+			// erreur lors de l'exécution : on ne retourne rien au client
 			catch(PDOException $e)
 			{
 				//$msg = 'PDO ERROR in ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
@@ -94,28 +105,32 @@
 			}
 		}
 		
+		// erreur dans les paramètres : on ne retourne rien au client
 		else
 		{
 			die();
 		}
 	}
 
+	// fonction de récupération d'une sauvegarde de jeu
 	function GetGame()
 	{
 		global $pdo;
 
+		// vérification de l'existence et de la validité des paramètres passés
 		if(isset($_GET["id"]) && $_GET["id"] != "" && strlen($_GET["id"]) == 6)
 		{
 			$id = strtoupper($_GET["id"]);
 			
 			//echo "GET GAME WITH ID $id";
 			
-			$sql = 'SELECT * FROM game WHERE id=:id';
+			$sql = 'SELECT * FROM game WHERE id=:id'; // code requête préparée
 			
 			try
 			{
 				$statement = $pdo->prepare($sql);
 				 
+				// exécution de la requête préparée
 				$statement->execute([
 					':id' => $id
 				]);
@@ -135,6 +150,7 @@
 				die(json_encode($arrAll[0]));
 			}
 			
+			// erreur dans la requête : on retourne une erreur 404 (objet demandé non trouvé)
 			catch(PDOException $e)
 			{
 				//$msg = 'PDO ERROR in ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
@@ -144,6 +160,7 @@
 			}
 		}
 		
+		// erreur dans les paramètres : on retourne une erreur 404 (objet demandé non trouvé)
 		else
 		{
 			header("HTTP/1.0 404 Not Found");
@@ -152,6 +169,7 @@
 	}
 	
 
+	// fonction de génération d'un identifiant "aléatoire"
 	function GenerateID()
 	{
 		//return "AAA000"; // pour tester les erreurs sans identifiant unique
